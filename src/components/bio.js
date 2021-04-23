972c71d017a501a5f0242e43fd888097230c9f15
blackzwashere <sapahub@gmail.com>
2021-04-23 23:09:56 +0800

    blog work

---
+++ src/components/bio.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import { rhythm } from "../utils/typography"
+
+const Bio = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "profile-pic.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 100) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div
+      style={{
+        display: `flex`,
+        marginBottom: rhythm(1),
+      }}
+      className="bio"
+    >
+      <div
+        style={{
+          marginRight: rhythm(1 / 2),
+          marginBottom: 0,
+          width: rhythm(2),
+          height: rhythm(2),
+          borderRadius: "50%",
+          overflow: "hidden",
+        }}
+      >
+        <Img fluid={data.placeholderImage.childImageSharp.fluid} alt="阿江" />
+      </div>
+      <p style={{ maxWidth: 250 }}>
+        Learn with <a href="https://twitter.com/capadesu">capadesu</a>
+        <br />
+        Smartest to become an idiot.
+      </p>
+    </div>
+  )
+}
+
+export default Bio
---
+++ src/pages/first-post/first-post.md
@@ -0,0 +1,20 @@
+---
+title: "The First Post"
+tags: ["Gatsby", "work"]
+date: "2021-04-20"
+spoiler: "The very first project"
+---
+
+![Gatsby](gatsby.gif)
+
+# This is the 1st post
+
+Built with gatsby, it's really really fast 🚀 exciting!
+
+> ### Fast in every way that matters
+>
+> Gatsby is a free and open source framework based on React that helps developers build blazing fast **websites** and **apps**
+
+Gatsby JS [Starter](https://www.gatsbyjs.org/starters/)
+
+🔥🔥🔥
